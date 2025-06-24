980daac3eddcb185d716a17c728c5994f914c43c
Humberto Tejada <humtej1204@gmail.com>
2025-06-23 22:49:01 -0500

    create library

---
+++ projects/ui-components/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ui-components
+ */
+
+export * from './lib/ui-components.service';
+
+export * from './lib/components/button/button.component';
+export * from './lib/components/card/card.component';
